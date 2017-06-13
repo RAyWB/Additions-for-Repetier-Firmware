d0d96bb1221584545fc12081a661d36f5e9c2e5a
Robert Ayrenschmalz <RAyWB@users.noreply.github.com>
2017-06-13 21:33:10 +0200

    added Empty template for G/M Code additions

---
+++ CustomEventsEmpty/CustomEventsImpl.h
@@ -0,0 +1,66 @@
+/*
+    This file is additional to Repetier-Firmware.
+
+    Repetier-Firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Repetier-Firmware is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Repetier-Firmware.  If not, see <http://www.gnu.org/licenses/>.
+
+    This firmware is a nearly complete rewrite of the sprinter firmware
+    by kliment (https://github.com/kliment/Sprinter)
+    which based on Tonokip RepRap firmware rewrite based off of Hydra-mmm firmware.
+
+    author of this additional File : RAyWB / Robert Ayrenschmalz
+
+*/
+#ifndef CustomEventsImpl_H
+#define CustomEventsImpl_H
+
+//#########################################################################################
+//#### GCode addition/replacement
+//#########################################################################################
+
+ bool Custom_GCode(GCode *com) 
+ {
+  
+    switch(com->G) {
+        //XXXX is the number you want to generate
+//		case XXXX: 
+		
+//		break;
+  
+  default:
+     return false;
+  }
+  return true;
+}
+
+//#########################################################################################
+//#### MCode addition/replacement
+//#########################################################################################
+
+
+bool Custom_MCode(GCode *com)
+{
+ 
+  switch(com->M) {
+
+        //XXXX is the number you want to generate
+//		case XXXX: 
+		
+//		break;    
+  default:
+     return false;
+  }
+  return true;
+}
+
+#endif
