d62cada71c305e5b5b49482170fb3bdbeb62fded
Robert Ayrenschmalz <RAyWB@users.noreply.github.com>
2017-06-20 19:12:17 +0200

    helping out :-)

---
+++ custom_ash/CustomEvents.h
@@ -0,0 +1,106 @@
+/*
+    This file is additional to Repetier-Firmware.
+
+    Repetier-Firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Repetier-Firmware is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Repetier-Firmware.  If not, see <http://www.gnu.org/licenses/>.
+
+    This firmware is a nearly complete rewrite of the sprinter firmware
+    by kliment (https://github.com/kliment/Sprinter)
+    which based on Tonokip RepRap firmware rewrite based off of Hydra-mmm firmware.
+
+    author of this additional File : ASH / Elias Taalab     thanks to RAyWB
+
+*/
+
+
+
+
+
+#define EmergencyOff_PIN     68     //digital input pin
+#define Emergency_Trigger_on      0    //low or high
+
+
+#ifndef CustomEvents_H
+#define CustomEvents_H
+
+extern bool Custom_GCode(GCode *com);
+extern bool Custom_MCode(GCode *com);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////// EEPROM POSITIONS //////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+#define epr_memoryX1                1200
+#define epr_memoryY1                1204
+#define epr_memoryZ1                1208
+
+#define epr_memoryX2                1212
+#define epr_memoryY2                1216
+#define epr_memoryZ2                1220
+
+#define epr_memoryX3                1224
+#define epr_memoryY3                1228
+#define epr_memoryZ3                1232
+
+#define epr_offsetX1                1236
+#define epr_offsetY1                1240
+#define epr_offsetZ1                1244
+
+#define epr_offsetX2                1248
+#define epr_offsetY2                1252
+#define epr_offsetZ2                1256
+
+#define epr_offsetX3                1260
+#define epr_offsetY3                1264
+#define epr_offsetZ3                1268
+
+
+extern void Custom_100MS();
+extern void Custom_INTIALIZE();
+extern void Emergency_Restore_IfNeeded();
+extern void Emergency_PowerOff_loop();
+
+#undef EVENT_TIMER_100MS
+#define EVENT_TIMER_100MS  {Custom_100MS();}
+
+#undef EVENT_INITIALIZE
+#define EVENT_INITIALIZE  {Custom_INTIALIZE();}
+
+#define epr_BackupTemp              1274
+#define epr_BackupTempB             1278
+#define epr_BackupFeedrate          1282
+#define epr_BackupSDposition        1286
+#define epr_EmergencyByte           1290
+#define epr_BackupExId              1292
+#define epr_Backup_OffsetX          1296
+#define epr_Backup_OffsetY          1300
+#define epr_Backup_OffsetZ          1304
+
+
+#define epr_BackupSDfilename        1308    //position of the first byte of the sd filename ,this will take positions to ~1330
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//G and M code replacements and /or additional
+#undef EVENT_UNHANDLED_G_CODE(c)
+#define EVENT_UNHANDLED_G_CODE(c) Custom_GCode(c)
+
+#undef EVENT_UNHANDLED_M_CODE(c)
+#define EVENT_UNHANDLED_M_CODE(c) Custom_MCode(c)
+
+
+#endif
---
+++ custom_ash/CustomEventsImpl.h
@@ -0,0 +1,351 @@
+/*
+    This file is additional to Repetier-Firmware.
+
+    Repetier-Firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Repetier-Firmware is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Repetier-Firmware.  If not, see <http://www.gnu.org/licenses/>.
+
+    This firmware is a nearly complete rewrite of the sprinter firmware
+    by kliment (https://github.com/kliment/Sprinter)
+    which based on Tonokip RepRap firmware rewrite based off of Hydra-mmm firmware.
+
+    author of this additional File : ASH / Elias Taalab     thanks to RAyWB
+
+*/
+
+
+
+
+
+
+#ifndef CustomEventsImpl_H
+#define CustomEventsImpl_H
+
+//#########################################################################################
+//#### GCode addition/replacement
+//#########################################################################################
+
+ bool Custom_GCode(GCode *com)
+ {
+
+    switch(com->G) {
+        //XXXX is the number you want to generate
+//		case XXXX:
+
+//		break;
+
+  default:
+     return false;
+  }
+  return true;
+}
+
+//#########################################################################################
+//#### MCode addition/replacement
+//#########################################################################################
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool Custom_MCode(GCode *com)
+{
+
+  switch(com->M) {
+
+
+    case 480:   // M480
+      #if EEPROM_MODE !=0
+       if(com->hasP()){
+        switch(com->P){
+
+		     case 1:
+          Commands::waitUntilEndOfAllMoves();
+          Printer::updateCurrentPosition(false);
+
+          HAL::eprSetFloat(epr_memoryX1,Printer::realXPosition());                   //store position from ram to eeprom
+          HAL::eprSetFloat(epr_memoryY1,Printer::realYPosition());
+          HAL::eprSetFloat(epr_memoryZ1,Printer::realZPosition());
+
+          HAL::eprSetFloat(epr_offsetX1,Printer::coordinateOffset[X_AXIS]);                   //store offset values from ram to eeprom
+          HAL::eprSetFloat(epr_offsetY1,Printer::coordinateOffset[Y_AXIS]);
+          HAL::eprSetFloat(epr_offsetZ1,Printer::coordinateOffset[Z_AXIS]);
+
+          Com::printF(PSTR("EEPROM Saved to position 1: X:"),HAL::eprGetFloat(epr_memoryX1));     //print some messages
+          Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_memoryY1));
+          Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_memoryZ1));
+
+          Com::printF(PSTR("Saved Offset 1: X:"),HAL::eprGetFloat(epr_offsetX1));
+          Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_offsetY1));
+          Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_offsetZ1));
+
+         break;
+
+         case 2:
+          Commands::waitUntilEndOfAllMoves();
+          Printer::updateCurrentPosition(false);
+
+          HAL::eprSetFloat(epr_memoryX2,Printer::realXPosition());                   //store position from ram to eeprom
+          HAL::eprSetFloat(epr_memoryY2,Printer::realYPosition());
+          HAL::eprSetFloat(epr_memoryZ2,Printer::realZPosition());
+
+          HAL::eprSetFloat(epr_offsetX2,Printer::coordinateOffset[X_AXIS]);                   //store offset values from ram to eeprom
+          HAL::eprSetFloat(epr_offsetY2,Printer::coordinateOffset[Y_AXIS]);
+          HAL::eprSetFloat(epr_offsetZ2,Printer::coordinateOffset[Z_AXIS]);
+
+
+          Com::printF(PSTR("EEPROM Saved to position 2: X:"),HAL::eprGetFloat(epr_memoryX2));
+          Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_memoryY2));
+          Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_memoryZ2));
+
+          Com::printF(PSTR("Saved Offset 2: X:"),HAL::eprGetFloat(epr_offsetX2));
+          Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_offsetY2));
+          Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_offsetZ2));
+
+         break;
+
+         case 3:
+          Commands::waitUntilEndOfAllMoves();
+          Printer::updateCurrentPosition(false);
+
+          HAL::eprSetFloat(epr_memoryX3,Printer::realXPosition());                   //store position from ram to eeprom
+          HAL::eprSetFloat(epr_memoryY3,Printer::realYPosition());
+          HAL::eprSetFloat(epr_memoryZ3,Printer::realZPosition());
+
+          HAL::eprSetFloat(epr_offsetX3,Printer::coordinateOffset[X_AXIS]);                   //store offset values from ram to eeprom
+          HAL::eprSetFloat(epr_offsetY3,Printer::coordinateOffset[Y_AXIS]);
+          HAL::eprSetFloat(epr_offsetZ3,Printer::coordinateOffset[Z_AXIS]);
+
+
+          Com::printF(PSTR("EEPROM Saved to position 3: X:"),HAL::eprGetFloat(epr_memoryX3));
+          Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_memoryY3));
+          Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_memoryZ3));
+
+          Com::printF(PSTR("Saved Offset 3: X:"),HAL::eprGetFloat(epr_offsetX3));
+          Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_offsetY3));
+          Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_offsetZ3));
+
+         break;
+
+              }
+         }
+         #else Com::printErrorF(Com::tNoEEPROMSupport);
+         #endif
+
+    break; //end  480
+
+//  ---------------------------------------------------------------------------------------------------------
+
+     case 481:  //M481
+      #if EEPROM_MODE !=0
+       if(com->hasP()){
+        switch(com->P){
+         case 1:
+            Com::printF(PSTR("Position 1: X:"),HAL::eprGetFloat(epr_memoryX1));
+            Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_memoryY1));
+            Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_memoryZ1));
+
+            Com::printF(PSTR("Offset 1: X:"),HAL::eprGetFloat(epr_offsetX1));
+            Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_offsetY1));
+            Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_offsetZ1));
+
+         break;
+
+         case 2:
+            Com::printF(PSTR("Position 2: X:"),HAL::eprGetFloat(epr_memoryX2));
+            Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_memoryY2));
+            Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_memoryZ2));
+
+            Com::printF(PSTR("Offset 2: X:"),HAL::eprGetFloat(epr_offsetX2));
+            Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_offsetY2));
+            Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_offsetZ2));
+
+
+         break;
+
+         case 3:
+            Com::printF(PSTR("Position 3: X:"),HAL::eprGetFloat(epr_memoryX3));
+            Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_memoryY3));
+            Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_memoryZ3));
+
+            Com::printF(PSTR("Offset 3: X:"),HAL::eprGetFloat(epr_offsetX3));
+            Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_offsetY3));
+            Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_offsetZ3));
+
+         break;
+
+            }
+         }
+         #else Com::printErrorF(Com::tNoEEPROMSupport);
+         #endif
+
+    break;  //end 481
+
+//  -----------------------------------------------------------------------------------------------------------------
+    case 482:   //M482
+    #if EEPROM_MODE !=0
+       if(com->hasP()){
+        switch(com->P){
+         case 1:
+
+            Printer::coordinateOffset[X_AXIS]= HAL::eprGetFloat(epr_offsetX1);                       //restore offset values
+            Printer::coordinateOffset[Y_AXIS]= HAL::eprGetFloat(epr_offsetY1);
+            Printer::coordinateOffset[Z_AXIS]= HAL::eprGetFloat(epr_offsetZ1);
+
+            Printer::moveToReal(HAL::eprGetFloat(epr_memoryX1)                                      //move to position stored in eeprom
+                                ,HAL::eprGetFloat(epr_memoryY1)
+                                ,HAL::eprGetFloat(epr_memoryZ1)
+                                ,IGNORE_COORDINATE
+                                ,(com->hasF() ? com->F : Printer::feedrate));
+            Printer::lastCmdPos[X_AXIS] = Printer::currentPosition[X_AXIS];// update gcode coords 
+            Printer::lastCmdPos[Y_AXIS] = Printer::currentPosition[Y_AXIS];// update gcode coords 
+            Printer::lastCmdPos[Z_AXIS] = Printer::currentPosition[Z_AXIS];// update gcode coords 
+
+         break;
+
+         case 2:
+
+            Printer::coordinateOffset[X_AXIS]= HAL::eprGetFloat(epr_offsetX2);                       //restore offset values
+            Printer::coordinateOffset[Y_AXIS]= HAL::eprGetFloat(epr_offsetY2);
+            Printer::coordinateOffset[Z_AXIS]= HAL::eprGetFloat(epr_offsetZ2);
+
+            Printer::moveToReal(HAL::eprGetFloat(epr_memoryX2)
+                                ,HAL::eprGetFloat(epr_memoryY2)
+                                ,HAL::eprGetFloat(epr_memoryZ2)
+                                ,IGNORE_COORDINATE
+                                ,(com->hasF() ? com->F : Printer::feedrate));
+            Printer::lastCmdPos[X_AXIS] = Printer::currentPosition[X_AXIS];// update gcode coords 
+            Printer::lastCmdPos[Y_AXIS] = Printer::currentPosition[Y_AXIS];// update gcode coords 
+            Printer::lastCmdPos[Z_AXIS] = Printer::currentPosition[Z_AXIS];// update gcode coords 
+                      
+
+         break;
+
+         case 3:
+
+            Printer::coordinateOffset[X_AXIS]= HAL::eprGetFloat(epr_offsetX3);                       //restore offset values
+            Printer::coordinateOffset[Y_AXIS]= HAL::eprGetFloat(epr_offsetY3);
+            Printer::coordinateOffset[Z_AXIS]= HAL::eprGetFloat(epr_offsetZ3);
+
+            Printer::moveToReal(HAL::eprGetFloat(epr_memoryX3)
+                                ,HAL::eprGetFloat(epr_memoryY3)
+                                ,HAL::eprGetFloat(epr_memoryZ3)
+                                ,IGNORE_COORDINATE
+                                ,(com->hasF() ? com->F : Printer::feedrate));
+            Printer::lastCmdPos[X_AXIS] = Printer::currentPosition[X_AXIS];// update gcode coords 
+            Printer::lastCmdPos[Y_AXIS] = Printer::currentPosition[Y_AXIS];// update gcode coords 
+            Printer::lastCmdPos[Z_AXIS] = Printer::currentPosition[Z_AXIS];// update gcode coords 
+                    
+
+         break;
+
+            }
+         }
+         #else Com::printErrorF(Com::tNoEEPROMSupport);
+         #endif
+      break;  //end 482
+//-----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+  default:
+     return false;
+  }
+
+
+  return true;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+void Custom_100MS(){
+  Emergency_PowerOff_loop();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+void Emergency_PowerOff_loop()                               // should run each 100MS
+{if (digitalRead(EmergencyOff_PIN)==Emergency_Trigger_on) {  //if power off triggerd (pin set to low)
+  if (sd.sdactive) {                                 //if printing from sd
+    Printer::kill(false);                //kill all ("false" mean kill all not only steppers)
+
+    HAL::eprSetFloat(epr_BackupFeedrate,Printer::feedrate);                          //backup all variables
+    HAL::eprSetInt32(epr_BackupSDposition,sd.sdpos);
+    HAL::eprSetFloat(epr_BackupExId,Extruder::current->id);
+    HAL::eprSetFloat(epr_BackupTemp,Extruder::current->tempControl.targetTemperatureC);
+    HAL::eprSetFloat(epr_BackupTempB,heatedBedController.targetTemperatureC);
+
+
+    HAL::eprSetFloat(epr_Backup_OffsetX,Printer::coordinateOffset[X_AXIS]);                   //backup offset values to eeprom
+    HAL::eprSetFloat(epr_Backup_OffsetY,Printer::coordinateOffset[Y_AXIS]);
+    HAL::eprSetFloat(epr_Backup_OffsetZ,Printer::coordinateOffset[Z_AXIS]);
+
+    for(int i=0;i<=21;i++)                                   //backup selected sd filename
+    {HAL::eprSetByte(epr_BackupSDfilename+i,Printer::printName[i]);}
+
+    HAL::eprSetByte(epr_EmergencyByte,1);     //set emergency byte to 1
+    }
+  else HAL::eprSetByte(epr_EmergencyByte,0);           // if the printer is not printing just set emergency byte to 0
+  }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Custom_INTIALIZE(){
+  pinMode(EmergencyOff_PIN,OUTPUT);
+  Emergency_Restore_IfNeeded();
+
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+void Emergency_Restore_IfNeeded(){                     //restore print if needed //should run on start
+
+  if(HAL::eprGetByte(epr_EmergencyByte)==1){                         //if emergency powered off detected
+
+      for(int i=0;i<=21;i++)                                                  //restore sd file name
+      {Printer::printName[i]=HAL::eprGetByte(epr_BackupSDfilename+i);}
+
+      sd.setIndex(HAL::eprGetInt32(epr_BackupSDposition));                                      // restore other variables
+     
+      GCode::executeFString(PSTR("G28"));                                   //first home the printer
+
+      Printer::feedrate=HAL::eprGetFloat(epr_BackupFeedrate);
+      Extruder::current->id=HAL::eprGetFloat(epr_BackupExId);
+      previousMillisCmd=HAL::timeInMilliseconds();
+
+      Printer::coordinateOffset[X_AXIS]= HAL::eprGetFloat(epr_Backup_OffsetX);                       //restore offset values
+      Printer::coordinateOffset[X_AXIS]= HAL::eprGetFloat(epr_Backup_OffsetY);
+      Printer::coordinateOffset[X_AXIS]= HAL::eprGetFloat(epr_Backup_OffsetZ);
+
+
+      Extruder::setTemperatureForExtruder(HAL::eprGetFloat(epr_BackupTemp),HAL::eprGetFloat(epr_BackupExId),0,true);
+      Extruder::setHeatedBedTemperature(HAL::eprGetFloat(epr_BackupTempB),0);
+    
+      HAL::eprSetByte(epr_EmergencyByte,0);                            // set emergency byte to 0
+
+      Printer::setMenuMode(MENU_MODE_PAUSED+MENU_MODE_SD_PRINTING,true);    //set the printer as it is printing and paused to enable the continue option in the menu
+     
+      Printer::moveToReal(HAL::eprGetFloat(epr_Backup_OffsetX), HAL::eprGetFloat(epr_Backup_OffsetY), HAL::eprGetFloat(epr_Backup_OffsetZ), IGNORE_COORDINATE, Printer::feedrate);
+      Printer::lastCmdPos[X_AXIS] = Printer::currentPosition[X_AXIS];// update gcode coords to startheight;
+      Printer::lastCmdPos[Y_AXIS] = Printer::currentPosition[Y_AXIS];// update gcode coords to startheight;
+      Printer::lastCmdPos[Z_AXIS] = Printer::currentPosition[Z_AXIS];// update gcode coords to startheight;
+        
+
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+#endif
